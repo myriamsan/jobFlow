450c5cd65a07337191b801b57fd7f7b52c19f052
myriamsan <myriam.sanvee@outlook.com>
2026-09-06 00:39:42 +0200

    User's email already use 409 exception raise

--- src/main/java/com/example/jobFlow/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/jobFlow/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.jobFlow.exceptions;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,5 +12,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        return new ResponseEntity<>("Cette ressource existe déjà ou viole une contrainte d'unicité", HttpStatus.CONFLICT);
     }
 }
